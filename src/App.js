03605aa9f7826da464abec6cf9a9758ff8961619
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-10-22 22:48:27 +0400

    #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [task, setTask] = useState("");
+  const [todoList, setTodoList] = useState(() => {
+    let todoList = null;
+    try {
+      todoList = JSON.parse(localStorage.getItem("todo"));
+    } catch (e) {}
+
+    return Array.isArray(todoList) ? todoList : [];
+  });
+
+  useEffect(() => {
+    const savedTodoList = localStorage.getItem("todo");
+    if (savedTodoList) {
+      setTodoList(JSON.parse(savedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todoList));
+  }, [todoList]);
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    const newTask = {
+      todo: task,
+      checked: false,
+    };
+
+    setTodoList([...todoList, newTask]);
+    setTask("");
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleClick}>
+        <input
+          name="task"
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Создать</button>
+      </form>
+      <div className="wrapper">
+        {todoList.map((item) => (
+          <div>
+            <p>{item.todo}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
